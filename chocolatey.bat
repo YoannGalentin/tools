afb00294322297fcd30540d61631826fa90761d7
YoannGalentin <yoann.galentin@gmail.com>
2020-08-13 17:01:48 +0200

    Create chocolatey.bat

---
+++ chocolatey.bat
@@ -0,0 +1,41 @@
+REM Microsoft Visual C++ Redistributable
+choco install vcredist140 -y
+
+REM Utilitaires windows
+choco install cmder -y
+choco install ditto -y
+choco install boostnote -y
+
+REM Outils collaboratif
+choco install zoom -y
+choco install mattermost-desktop -y
+
+REM Editeurs, IDE
+choco install vscode -y
+choco install jetbrainstoolbox -y
+
+REM Language, platform
+choco install golang -y
+choco install cloudfoundry-cli -y
+
+REM Outils
+choco install git -y
+choco install winscp.portable -y
+choco install putty -y
+choco install postman -y
+choco install soapui -y
+choco install chromium -y
+choco install mremoteng -y
+choco install dotnet4.7 -y
+choco install paint.net -y
+choco install xmind -y
+choco install notepadplusplus -y
+choco install vlc -y
+choco install curl -y
+choco install python2 -y
+choco install python -y
+choco install cloudfoundry-cli -y
+choco install adobereader -y
+
+REM Docker
+choco install docker-desktop -y
