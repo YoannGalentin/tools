736867452b4b5fc76c282ee9e350a3b51905dd27
Yoann Galentin <yoann.galentin@gmail.com>
2020-08-15 10:44:36 +0200

    WIP

--- setup.sh
+++ setup.sh
@@ -1,63 +1,42 @@
 #!/usr/bin/env bash
+
 #
 # setup.sh: run the workstation setup on Ubuntu's WSL
+#
 
-sudo apt install zsh
-sudo apt-get install powerline fonts-powerline
-
-
-
-echo
-echo "git install"
-echo
-sudo apt-get install git
-
-# ZSH
-echo
-echo "ZSH install"
-echo
-sudo apt-get install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# using dircolors.ansi-dark
-# curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark --output ~/.dircolors
-
+# Update distro
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
 
 echo
-echo "Jenv install"
+echo "Tools install (git, ansible, vim, ...)"
 echo
-echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(jenv init -)"' >> ~/.zshrc
+sudo apt-get install -y git
+sudo apt-get install -y vim  ansible git
 
-source /home/yoyo/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# Fonts
+echo
+echo "Fonts"
+echo
+sudo apt-get install -y powerline fonts-powerline
 
-
-echo ""
-## set colors for LS_COLORS
-eval `dircolors ~/.dircolors`
-
-
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# export CHROME_BIN='/usr/bin/chromium-browser'
-export CHROME_BIN='/usr/bin/google-chrome-stable'
-
-export GOPATH=$HOME/go
-export GOBIN="$GOPATH/bin"
-
+#
+#echo
+#echo "Jenv install"
+#echo
+#echo 'export PATH="$HOME/.jenv/bin:$PATH"' >> ~/.zshrc
+#echo 'eval "$(jenv init -)"' >> ~/.zshrc
+#
+#source /home/yoyo/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # NVM
+echo
+echo "NVM install"
+echo
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.bashrc
 nvm install node
-
-# Yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
-
 
 # Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -65,4 +44,33 @@ sudo sh get-docker.sh
 
 # Allow your user to access the Docker CLI without needing root access.
 sudo usermod -aG docker $USER
+
+# using dircolors.ansi-dark
+# curl https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark --output ~/.dircolors
+#echo ""
+### set colors for LS_COLORS
+#eval `dircolors ~/.dircolors`
+
+# Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install --no-install-recommends -y yarn
+
+# ZSH
+echo
+echo "ZSH install"
+echo
+sudo apt-get install -y zsh
+chsh -s $(which zsh) # change default to zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo
+echo "ZSH Plugins install"
+echo
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+
+sudo chmod -R 755 /home/yoyo/.oh-my-zsh/plugins/
+touch ~/.z
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
